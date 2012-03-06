b1b446db1fccdbac52927572e2625a79113a3623
Gosuke Miyashita <gosukenator@gmail.com>
2012-03-07 00:10:51 +0900

    Add EXIF tag plugin

---
+++ exif_tag.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# A Liquid tag rof Jekyll sites that allows showing exif data.
+# by: Gosuke Miyashita
+#
+# Example usage: {% exif /images/sample.jpg %}
+
+require 'exifr'
+
+module Jekyll
+  class ExifTag < Liquid::Tag
+    def initialize(tag_name, file, token)
+      super
+      @image_file = File.expand_path "../source" + file.strip , File.dirname(__FILE__)
+    end
+
+    def render(context)
+      exif = EXIFR::JPEG::new(@image_file)
+      <<-HTML
+焦点距離 #{exif.focal_length.to_i} mm F#{sprintf "%.1f", exif.f_number.to_f} #{exif.model} ISO #{exif.iso_speed_ratings} #{exif.exposure_time.to_i} 秒露光
+      HTML
+    end
+  end
+end
+
+Liquid::Template.register_tag('exif', Jekyll::ExifTag)

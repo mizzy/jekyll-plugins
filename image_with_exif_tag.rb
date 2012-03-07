c60a91c34cd6de339cd3f6fb36f20e05b831f4ce
Gosuke Miyashita <gosukenator@gmail.com>
2012-03-07 23:36:20 +0900

    Add image_with_exif_tag.rb

---
+++ image_with_exif_tag.rb
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# Title: Simple Image tag for Jekyll + Setting EXIF data in tile and alt
+# Authors: Brandon Mathis http://brandonmathis.com
+#          Felix Schäfer, Frederic Hemberger
+# Edited by: Gosuke Miyashita
+# Description: Easily output images with optional class names, width, height, title and alt attributes
+#
+# Syntax {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | "title text" ["alt text"]] %}
+#
+# Examples:
+# {% img /images/ninja.png Ninja Attack! %}
+# {% img left half http://site.com/images/ninja.png Ninja Attack! %}
+# {% img left half http://site.com/images/ninja.png 150 150 "Ninja Attack!" "Ninja in attack posture" %}
+#
+# Output:
+# <img src="/images/ninja.png">
+# <img class="left half" src="http://site.com/images/ninja.png" title="Ninja Attack!" alt="Ninja Attack!">
+# <img class="left half" src="http://site.com/images/ninja.png" width="150" height="150" title="Ninja Attack!" alt="Ninja in attack posture">
+#
+
+require 'exifr'
+
+module Jekyll
+
+  class ImageTag < Liquid::Tag
+    @img = nil
+
+    def initialize(tag_name, markup, tokens)
+      attributes = ['class', 'src', 'width', 'height', 'title']
+
+      if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
+        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
+        if /(?:"|')(?<title>[^"']+)?(?:"|')\s+(?:"|')(?<alt>[^"']+)?(?:"|')/ =~ @img['title']
+          @img['title']  = title
+          @img['alt']    = alt
+        else
+          @img['alt']    = @img['title'].gsub!(/"/, '&#34;') if @img['title']
+        end
+        @img['class'].gsub!(/"/, '') if @img['class']
+      end
+
+      if @img['src'] !~ /^https?:\/\//
+        @image_file = File.expand_path "../source" + @img['src'] , File.dirname(__FILE__)
+        exif = EXIFR::JPEG::new(@image_file)
+        if exif.exif?
+          title =
+            sprintf "Focal Length %.1f mm F%.1f %s ISO %s Exposure Time %s second %s",
+            exif.focal_length.to_i, exif.f_number.to_f, exif.model,
+            exif.iso_speed_ratings, exif.exposure_time.to_i, exif.date_time_original
+          @img['title'] = @img['alt'] = title
+        end
+      end
+
+      super
+    end
+
+    def render(context)
+      if @img
+        "<img #{@img.collect {|k,v| "#{k}=\"#{v}\"" if v}.join(" ")}>"
+      else
+        "Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \"title text\" [\"alt text\"]] %}"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('img_with_exif', Jekyll::ImageTag)
